db66965f221030d6878c5e5cfa5461de9fffa2b5
Daniel Ceinos <daniel.ceinos@gmail.com>
2021-01-24 12:54:32 +0100

    random avatar and name

--- botMain.py
+++ botMain.py
@@ -15,6 +15,15 @@ def log(msg):
 @bot.event
 async def on_ready():
     await bot.change_presence(activity = discord.Game(name = "!help"))
+    while True:
+        bot_names = open('usernames.txt', 'r').readlines()
+        name = random.choice(bot_names).strip()
+        avatars = os.listdir('./avatar/')
+        avatar = random.choice(avatars)
+        fp = open('./avatar/{}'.format(avatar), 'rb')
+        pfp = fp.read()
+        await bot.user.edit(username=name, avatar=pfp)
+        await asyncio.sleep(60*60*24)
 
 @bot.command(help = 'Reproduce un sonido en el canal de voz en el que se encuentre el usuario')
 async def play(ctx, sound):
@@ -25,8 +34,9 @@ async def play(ctx, sound):
         await vc.disconnect()
     log('Called play with {0}'.format(sound))
     channel = ctx.author.voice.channel
+    log('tryin to connect to {0}'.format(channel))
     vc =  await channel.connect()
-    player = vc.play(discord.FFmpegOpusAudio('./{0}.mp3'.format(sound)))
+    player = vc.play(discord.FFmpegOpusAudio('./sounds/{0}.mp3'.format(sound)))
     guild_vc[ctx.guild.id] = vc
     while vc.is_playing():
         await sleep(1)
@@ -45,7 +55,7 @@ async def stop(ctx):
 @bot.command(help = 'Devuelve la lista de sonidos disponibles para reproucir')
 async def sounds(ctx):
     log('Called sounds')
-    files = [f.replace('.mp3', '') for f in os.listdir('.') if os.path.isfile(f) and 'mp3' in f]
+    files = [f.replace('.mp3', '') for f in os.listdir('./sounds/') if 'mp3' in f]
     await ctx.send('Sonidos disponibles:\n {0}'.format(files))
 
 @bot.command(help = 'Añade un nuevo sonido mp3')
@@ -82,8 +92,8 @@ async def playyt(ctx, link):
     if valid_url:
         guild_id = ctx.guild.id
         yt_file_name = "yt-{0}".format(guild_id)
-        os.system('rm {0}.mp3'.format(yt_file_name))
-        os.system('youtube-dl --extract-audio --audio-format mp3 -o "{0}.mp3" {1}'.format(yt_file_name, link))
+        os.system('rm ./sounds/{0}.mp3'.format(yt_file_name))
+        os.system('youtube-dl --extract-audio --audio-format mp3 -o "./sounds/{0}.mp3" {1}'.format(yt_file_name, link))
         print('Downloaded {}'.format(yt_file_name))
         await play(ctx, yt_file_name)
     else:
@@ -93,16 +103,16 @@ async def playyt(ctx, link):
 @bot.command()
 async def remember(ctx, msg, time):
     await ctx.message.delete()
+    dm_channel = await ctx.message.author.create_dm()
     try:
         scheduled_time = parse(time)
         td = scheduled_time - datetime.datetime.now()
         seconds = td.seconds
         print("Scheduled {0} at {1}".format(msg, scheduled_time))
         await asyncio.sleep(seconds)
-        await ctx.send("{0} you told me to remember this:\n> {1}".format(ctx.message.author.mention, msg))
+        await dm_channel.send("{0} you told me to remember this:\n> {1}".format(ctx.message.author.mention, msg), tts=True)
     except Exception as inst:
         print(inst)
-        dm_channel = await ctx.message.author.create_dm()
         await dm_channel.send('Malformed date {0}'.format(time))
 
 print('Init')
